a65a7d88646f7f26bfd428d7f95fec51ea350bc9
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2023-08-04 17:48:49 +0530

    Auth Completed

--- src/app/dashboard/login/page.jsx
+++ src/app/dashboard/login/page.jsx
@@ -1,12 +1,40 @@
-"use client";
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import styles from "./page.module.css";
-import { signIn } from "next-auth/react";
+import Link from "next/link";
+// import { useRouter } from "next/navigation";
+import {signIn} from "next-auth/react"
 
 const Login = () => {
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn("credentials", {email, password})
+    
+
+  }
   return (
     <div className={styles.container}>
-      <button onClick={() => signIn("google")}>Login with Google</button>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className={styles.input}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.input}
+          required
+        />
+        <button className={styles.button}>Login</button>
+      </form>
+      <Link href="/dashboard/login">Login with an existing account!</Link>
     </div>
   );
 };
